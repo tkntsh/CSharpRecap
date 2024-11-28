32d0f32c3f793fde86d740ba400fed61562291cd
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:31:03 +0200

    Create recap6.cs
    Create a variable named state and assign it the value 0.
    Write an if statement that checks if state is equal to 1.
    Produce a result that returns a value of The switch is OFF:
    Is the return value true or false?

---
+++ recap6.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int state = 0;
+            if(state == 1)
+            {
+                Console.WriteLine("The switch is OFF");
+            }
+            else
+            {
+                Console.WriteLine("The switch if ON");
+            }
+        }
+    }
+}
