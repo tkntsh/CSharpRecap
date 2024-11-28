e853f94a4c2ea0749413566223343524bd2156e3
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:28:55 +0200

    Create recap4.cs
    Create two variables named scores and average score.
    Store the sum of 15, 54, 19, 43 to the variable scores and the calculation of their average to average score variable.
    Call your calculation variable.
    What is the answer?

---
+++ recap4.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int score = 15+54+19+43;
+            int avgScore = score/4;
+
+            Console.WriteLine(avgScore);
+        }
+    }
+}
