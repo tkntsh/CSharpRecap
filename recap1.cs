4c235eb03d3e45125e098da54370a9995957b039
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:26:56 +0200

    Create recap1.cs
    Create a variable for time of the meeting and print the following:
    
    The meeting is at 7am

---
+++ recap1.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string meeting = "7am";
+            Console.WriteLine("The meeting is at " + meeting);
+        }
+    }
+}
