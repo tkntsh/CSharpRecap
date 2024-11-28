e715b87311b19158beb6d2b8b703946637de0331
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:31:32 +0200

    Create recap7.cs
    Print the sum of 5 multiplied by 80 divided by 2 to Console.
    What is the answer?

---
+++ recap7.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(5 * (80 / 2));
+        }
+    }
+}
+
