8b5ca39dfc734953569ac6576a62bfe9a2131531
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:28:22 +0200

    Create recap3.cs
    Create two variables named destination and next destination.
    Assign values to both variables.
    Produce the following output:
    
    Currently I am in Sauze D'Oulx,
    I would like to go to Beaver Creek next.

---
+++ recap3.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = "Los Angeles";
+            string nextDestination = "Munich";
+
+            Console.WriteLine("Currently I am in " + destination + ",\nI would like to go to " + nextDestination + " next");
+        }
+    }
+}
