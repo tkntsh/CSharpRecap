3fd408f290624ac3c64ffcbabd28bd13225727b4
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:27:26 +0200

    Create recap2.cs
    Write a statement to find if 5 multiplied by 2 is not equal to 10.
    What is the answer?

---
+++ recap2.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (5 * 2 != 10)
+            {
+                Console.WriteLine("false");
+            }
+            else
+            {
+                Console.WriteLine("true");
+            }
+        }
+    }
+}
