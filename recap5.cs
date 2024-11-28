0ca77565fa98a20a465f84196cec601366b437ee
Ntokozo <ntokntshangase@gmail.com>
2024-11-28 16:30:31 +0200

    Create recap5.cs
    Create a log which produces the following:
    
    Howard: "I thought you didn't like Facebook any more?"
    Sheldon: "Don't be silly, I'm a fan of anything that tries to replace actual human contact."

---
+++ recap5.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Howard: \"I thought you didn't like Facebook any more?\"\nSheldon: \"Don't be silly, I'm a fan of anything that tries to replace actual human contact.\"");
+        }
+    }
+}
